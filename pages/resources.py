3d7066b2a823dde434de48b2b132872407f6edb1
Richard Lieu <rlieu@dunmorecap.com>
2022-10-16 18:47:09 -0700

    resources page

---
+++ pages/resources.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.markdown('# Resources')
+
+st.markdown("""
+## Project
+- [Source Code](https://github.com/rlieu002/milestoneI/pull/14)
+- [Deepnote](https://deepnote.com/workspace/milestonei-1718-e97dd873-3dfa-452c-ad11-2e190d190593/project/Team-Project-d4e15c74-6173-42e1-9532-632717f41fb2/%2FmilestoneI%2Finflation-intro.ipynb)
+
+## Data
+- [Federal Reserve](https://www.federalreserve.gov/monetarypolicy/openmarket.htm)
+- [bea](https://apps.bea.gov/iTable/iTable.cfm?reqid=19&step=2#reqid=19&step=2&isuri=1&1921=survey)
+- [FRED](https://fred.stlouisfed.org/)
+""")
